c3f5ceccf4ac4b9cbc26b990ed3e1aaffe4488d7
RuLe0G <khlebnikov2015@yandex.ru>
2020-11-14 15:01:56 +0700

    Initial commit

---
+++ лаб4/Form1.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace лаб4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.imageBox1 = new Emgu.CV.UI.ImageBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.imageBox2 = new Emgu.CV.UI.ImageBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button16 = new System.Windows.Forms.Button();
+            this.button17 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button7 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imageBox1
+            // 
+            this.imageBox1.Location = new System.Drawing.Point(-4, -4);
+            this.imageBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.imageBox1.Name = "imageBox1";
+            this.imageBox1.Size = new System.Drawing.Size(640, 480);
+            this.imageBox1.TabIndex = 2;
+            this.imageBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(644, 13);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(191, 28);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Загрузить фото";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // imageBox2
+            // 
+            this.imageBox2.Location = new System.Drawing.Point(843, 0);
+            this.imageBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.imageBox2.Name = "imageBox2";
+            this.imageBox2.Size = new System.Drawing.Size(640, 480);
+            this.imageBox2.TabIndex = 2;
+            this.imageBox2.TabStop = false;
+            // 
+            // button16
+            // 
+            this.button16.Location = new System.Drawing.Point(644, 312);
+            this.button16.Margin = new System.Windows.Forms.Padding(4);
+            this.button16.Name = "button16";
+            this.button16.Size = new System.Drawing.Size(100, 28);
+            this.button16.TabIndex = 25;
+            this.button16.Text = "Сброс";
+            this.button16.UseVisualStyleBackColor = true;
+            this.button16.Click += new System.EventHandler(this.button16_Click);
+            // 
+            // button17
+            // 
+            this.button17.Location = new System.Drawing.Point(752, 312);
+            this.button17.Margin = new System.Windows.Forms.Padding(4);
+            this.button17.Name = "button17";
+            this.button17.Size = new System.Drawing.Size(83, 28);
+            this.button17.TabIndex = 27;
+            this.button17.Text = "SAVE";
+            this.button17.UseVisualStyleBackColor = true;
+            this.button17.Click += new System.EventHandler(this.button17_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(645, 108);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(191, 26);
+            this.button2.TabIndex = 28;
+            this.button2.Text = "triangle";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(645, 140);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(191, 26);
+            this.button3.TabIndex = 28;
+            this.button3.Text = "rectangle";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(643, 204);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(191, 26);
+            this.button4.TabIndex = 28;
+            this.button4.Text = "circle";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(645, 48);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(83, 22);
+            this.textBox1.TabIndex = 29;
+            this.textBox1.Text = "80";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(752, 48);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(83, 22);
+            this.textBox2.TabIndex = 29;
+            this.textBox2.Text = "50";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(643, 76);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(191, 26);
+            this.button5.TabIndex = 28;
+            this.button5.Text = "Tri + Rec";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(645, 172);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(39, 22);
+            this.textBox3.TabIndex = 29;
+            this.textBox3.Text = "36";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(690, 172);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(39, 22);
+            this.textBox4.TabIndex = 29;
+            this.textBox4.Text = "2";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(735, 172);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(39, 22);
+            this.textBox5.TabIndex = 29;
+            this.textBox5.Text = "500";
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(643, 264);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(191, 23);
+            this.button6.TabIndex = 30;
+            this.button6.Text = "По цвету";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(645, 236);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(39, 22);
+            this.textBox6.TabIndex = 29;
+            this.textBox6.Text = "30";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(790, 379);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 17);
+            this.label1.TabIndex = 31;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(790, 411);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 17);
+            this.label2.TabIndex = 31;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(699, 357);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 32;
+            this.button7.Text = "button7";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(1924, 493);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button17);
+            this.Controls.Add(this.button16);
+            this.Controls.Add(this.imageBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.imageBox1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Emgu.CV.UI.ImageBox imageBox1;
+        private System.Windows.Forms.Button button1;
+        private Emgu.CV.UI.ImageBox imageBox2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button16;
+        private System.Windows.Forms.Button button17;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button7;
+    }
+}
+
---
+++ лаб4/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace лаб4
+{
+    public partial class Form1 : Form
+    {
+
+        private UCDLRAOCI myclass = new UCDLRAOCI();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //открыть картинку
+
+        private string OpenFile()
+        {
+            string fileName = null;
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = ("Файлы изображений | *.jpg; *.jpeg; *.jpe; *.jfif; *.png");
+            var result = openFileDialog.ShowDialog(); // открытие диалога выбора файла            
+            if (result == DialogResult.OK) // открытие выбранного файла
+            {
+                fileName = openFileDialog.FileName;
+            }
+            return fileName;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            myclass.Source(OpenFile());
+            imageBox1.Image = myclass.sourceImage;
+            imageBox2.Image = myclass.sourceImage;
+        }
+        private void button16_Click(object sender, EventArgs e)
+        {
+            myclass.MainImage();
+            imageBox2.Image = myclass.MainImageExp;
+            myclass.ShC = 0;
+            myclass.ShT = 0;
+            myclass.ShR = 0;
+            label1.Text = "";
+            label2.Text = "";
+        }
+        private void button17_Click(object sender, EventArgs e)
+        {
+
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = ("Файлы изображений | *.jpg; *.jpeg; *.jpe; *.jfif; *.png");
+            var result = saveFileDialog.ShowDialog(); // открытие диалога выбора файла            
+            if (result == DialogResult.OK) // открытие выбранного файла
+            {
+                string fileName = saveFileDialog.FileName;
+                myclass.saveJpeg(fileName);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            imageBox2.Image = myclass.Lab4Func(3,Convert.ToDouble(textBox1.Text.ToString()), Convert.ToDouble(textBox2.Text.ToString()));
+            label1.Text = myclass.ShT.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            imageBox2.Image = myclass.Lab4Func(4, Convert.ToDouble(textBox1.Text.ToString()), Convert.ToDouble(textBox2.Text.ToString()));
+            label1.Text = myclass.ShR.ToString();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            imageBox2.Image = myclass.Lab4Func(0, Convert.ToDouble(textBox1.Text.ToString()), Convert.ToDouble(textBox2.Text.ToString()));
+            label1.Text = myclass.ShR.ToString();
+            label2.Text = myclass.ShT.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            imageBox2.Image = myclass.Lab4FuncCircl();
+            label1.Text = myclass.ShC.ToString();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            imageBox2.Image = myclass.Lab4FuncColor(Convert.ToByte(textBox6.Text.ToString()));
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            imageBox2.Image = myclass.CannyProcess();
+        }
+    }
+}
